c7444aa40a370e1b501c76c8f5f6474dbeb78db4
Pavel Beletsky <pavel.beletsky@live.com>
2019-12-10 22:36:56 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(input) {
+
+  let i = parseInt(input.split(":")[0]+input.split(":")[1], 10)
+  if (i > 0 && i < 1200) return "Good Morning"
+  else if (i >= 1200 && i <= 1700) return "Good Afternoon"
+  else if (i > 1700 && i <= 2359) return "Good Evening"
+  else try {
+    throw new Error('Whoops!');
+  } catch (e) {
+    return `${e.name}` + `: ` + `${e.message}`
+  } 
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  let node = document.querySelector("#greeting");
+  node.innerText = str;
+}
